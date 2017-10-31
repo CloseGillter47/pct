ee726ce7cf7ac8c7e8c2a7fda1b73c7c8a606900
CloseGillter47 <LiJQ2016@gmail.com>
2017-10-31 15:51:55 +0800

    完成基本的项目结构

--- src/components/sys/Header.vue
+++ src/components/sys/Header.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="hello">
+  <div class="header-box">
     
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "InsoHeader",
   data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
-  }
+    return {};
+  },
+
+  components: {}
 };
 </script>
-<style lang="sass" scoped>
-
+<style lang="scss">
+.header-box {
+  width: 100%;
+  height: 100%;
+  max-height: 54px;
+  background: #409eff;
+  z-index: 1000;
+  
+  -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),
+    0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
 </style>
 
 
--- src/pages/index-v.vue
+++ src/pages/index-v.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="hello">
-    123
+  <div class="header-layout">
+    <inso-header></inso-header>
   </div>
 </template>
 
 <script>
+import InsoHeader from "../components/sys/Header";
+
 export default {
   name: "HelloWorld",
   data() {
     return {
       msg: "Welcome to Your Vue.js App"
     };
+  },
+
+  components: {
+    InsoHeader
   }
 };
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+.header-layout {
+  width: 100%;
+  height: 96px;
+  position: relative;
+}
 </style>
 
 <style scoped>
