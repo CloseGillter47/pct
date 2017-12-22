be97aea35c40c48916d34d552e6888c64a62930e
CloseGillter47 <CloseGillter47@gmail.com>
2017-12-22 11:49:04 +0800

    完善Badge组件（基本没问题了）

---
+++ src/utils/kurokoArray.js
@@ -0,0 +1,23 @@
+/**
+ * 个人工具集
+ */
+
+
+/**
+ * @description 检验某个对象是否为数组里的一员
+ * @param  {Array} list
+ * @param  {Any} item
+ * @returns Boolean 
+ */
+export function oneOfthen(list, item) {
+
+}
+
+/**@description 检验两个对象是否相同
+ * @param  {Object|Any} obj1
+ * @param  {Object|Any} obj2
+ * @returns Boolean
+ */
+export function theSameAs(obj1, obj2) {
+  return Object.is(obj1, obj2);
+}
