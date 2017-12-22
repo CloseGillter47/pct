9465c850a0956c861847beac70ee62f51973c625
CloseGillter47 <CloseGillter47@gmail.com>
2017-12-22 16:17:02 +0800

    ss

--- src/components/sys/Menu.vue
+++ src/components/sys/Menu.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="kuroko-menu-box">
    <ul>
-     <li><div>标题1</div></li>
-     <li><div>标题2</div></li>
-     <li><div>标题3</div></li>
-     <li><div>标题4</div></li>
+     <li class="kuroko-menu-item">
+       <div>
+         <span class="menu-icon"><i class="mdi mdi-home"></i></span>
+         <span>标题1</span>
+         <span class="menu-arrow"><i class="mdi mdi-chevron-down"></i></span>
+       </div>
+     </li>
+     <li class="kuroko-menu-item">
+       <div>标题2</div>
+     </li>
+     <li class="kuroko-menu-item">
+       <div>标题3</div>
+     </li>
+     <li class="kuroko-menu-item">
+       <div>标题4</div>
+     </li>
      <li>
        <div>标题5</div>
        <ul>
-         <li><div>标题5.1</div></li>
-         <li><div>标题5.2</div></li>
+         <li class="kuroko-menu-item"><div>标题5.1</div></li>
+         <li class="kuroko-menu-item"><div>标题5.2</div></li>
        </ul>
      </li>
    </ul>
@@ -33,7 +45,22 @@ export default {
   background: wheat;
   ul {
     list-style: none;
-    padding-left:20px;
+    padding: 0px;
+    color: #fff;
+    li {
+      background: gray;
+      padding: 0px;
+      & > div {
+        padding: 8px 20px;
+        font-size: 14px;
+        cursor: default;
+      }
+    }
+  }
+}
+.kuroko-menu-item {
+  &:hover {
+    background: rgb(97, 97, 97);
   }
 }
 </style>
